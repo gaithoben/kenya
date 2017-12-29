b326090367dd03ea6e23cd18158b9dbf981b7f8e
Bernard Gaitho <gaithoben@gmail.com>
2017-12-29 11:52:39 +0300

    feat(randomcounty, randomconstituency): Added Constituencies to the project, link to eact respective
    closes #2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 var counties = require('./json/counties.json');
+var constituencies = require('./json/constituencies.json');
 var uniqueRandomArray = require('unique-random-array');
 
 module.exports = {
   counties: counties,
-  randomcounty: uniqueRandomArray(counties),
+  randomcounty: randomcounty,
+  constituencies: constituencies,
+  randomconstituency: randomconstituency,
 };
+
+function randomcounty(number) {
+  var arr = uniqueRandomArray(counties);
+  if (number === undefined) {
+    return arr();
+  } else {
+    var randomitems = [];
+    for (var i = 0; i < number; i++) {
+      randomitems.push(arr());
+    }
+    return randomitems;
+  }
+}
+
+function randomconstituency(number) {
+  var arr = uniqueRandomArray(constituencies);
+  if (number === undefined) {
+    return arr();
+  } else {
+    var randomitems = [];
+    for (var i = 0; i < number; i++) {
+      randomitems.push(arr());
+    }
+    return randomitems;
+  }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -18,5 +18,41 @@ describe('Counties List', function() {
       var randomcounty = kenya.randomcounty();
       expect(kenya.counties).to.include(randomcounty);
     });
+
+    it('Should return an array of random Counties if passed a number', function() {
+      var randomcounties = kenya.randomcounty(3);
+      expect(randomcounties).to.have.length(3);
+      randomcounties.forEach(function(item) {
+        expect(kenya.counties).to.include(item);
+      });
+    });
+  });
+});
+
+describe('Constituencies List', function() {
+  describe('constituencies', function() {
+    it('Should be an array of objects', function() {
+      expect(kenya.constituencies).to.satisfy(arraOfObjects);
+
+      function arraOfObjects(args) {
+        return typeof args === 'object';
+      }
+    });
+  });
+
+  describe('Random constituency', function() {
+    it('Should return a random constituency from the list of all constituencies', function() {
+      var randomconstituency = kenya.randomconstituency();
+      console.log('CONSTITUENCY', randomconstituency);
+      expect(kenya.constituencies).to.include(randomconstituency);
+    });
+
+    it('Should return an array of random items if passed a number', function() {
+      var randomconstituencies = kenya.randomconstituency(3);
+      expect(randomconstituencies).to.have.length(3);
+      randomconstituencies.forEach(function(item) {
+        expect(kenya.constituencies).to.include(item);
+      });
+    });
   });
 });
